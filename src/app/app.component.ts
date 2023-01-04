f6df27f621f720ddc59b2513ef50bcf22ab46e34
ShadowQueen <feniksreborn@gmail.com>
2023-01-04 15:28:58 +0100

    sec init

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'test-barcode-scanner';
+
+  value: string = '';
+  isError = false;
+
+  onError(error: any) {
+    console.error(error);
+    this.isError = true;
+  }
+  
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgxBarcodeScannerModule } from '@eisberg-labs/ngx-barcode-scanner';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxBarcodeScannerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
